26101eeb545bbb9a6955140ceaa8c91b9cf27ef6
Ash1sc <undefeated2001@outlook.com>
2023-10-16 22:56:02 +0800

    add beautify function

---
+++ DataAnalysisForPython/test/__init__.py
@@ -0,0 +1,8 @@
+# add python module path
+import sys
+import os
+sys.path.append(os.getcwd())
+
+# for beatifying the output of datasets in the book.
+import util.setup
+util.setup.beatify_output()
--- DataAnalysisForPython/util/setup.py
+++ DataAnalysisForPython/util/setup.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 
-if __name__ != "__main__":
+
+def beatify_output() -> None:
     pd.options.display.max_columns = 20
     pd.options.display.max_rows = 20
     pd.options.display.max_colwidth = 80
     np.set_printoptions(precision=4, suppress=True)
+    print("This is not main")
